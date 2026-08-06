3bccf47e50ac30a0211141740a409f57e62d3fea
Yusuf Abass Ali <aliyusufabass@gmail.com>
2026-08-06 11:57:19 +0300

    Fix Django initialization order in ASGI

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,16 +1,21 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
-from manyumbu10.routing import websocket_urlpatterns
-from manyumbu10.websocket_auth import JWTAuthMiddleware
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
+from django.core.asgi import get_asgi_application
+
+# Hii lazima iitwe kabla ya ku-import routing/consumers
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": JWTAuthMiddleware(URLRouter(websocket_urlpatterns)),
-})
+from channels.routing import ProtocolTypeRouter, URLRouter
+from manyumbu10.websocket_auth import JWTAuthMiddleware
+from manyumbu10.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": JWTAuthMiddleware(
+            URLRouter(websocket_urlpatterns)
+        ),
+    }
+)
